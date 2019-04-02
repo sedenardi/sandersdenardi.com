ff8f5df977e7c56bb5e7c7f15d48900be8a27880
Sanders DeNardi <sedenardi@gmail.com>
2019-04-02 19:22:51 -0400

    use hooks and functional components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,94 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import githubIcon from '../assets/github-square.svg';
 import twitterIcon from '../assets/twitter-square.svg';
 import envelopeIcon from '../assets/envelope-square.svg';
 
-class Nav extends React.Component {
-  state = {
-    expand: false
-  }
-  toggleExpand = () => { this.setState({ expand: !this.state.expand }); }
-  projects() {
-    if (this.props.projects && this.props.projects.length) {
-      let lsCls = 'tw-list-reset lg:tw-pl-4 lg:tw-block';
-      if (!this.state.expand) {
-        lsCls += ' tw-hidden';
-      }
-      return (
-        <div className="tw-text-center lg:tw-text-left tw-text-2xl tw-font-thin tw-pt-4 lg:tw-pt-8">
-          <a className="tw-cursor-pointer lg:tw-cursor-default tw-pt-4" onClick={this.toggleExpand}>
-            <span className="tw-hidden lg:tw-inline">Projects</span>
-            <span className="lg:tw-hidden">Projects {this.state.expand ? '▲' : '▼'}</span>
-          </a>
-          <ul className={lsCls}>
-            {this.props.projects.map((p) => {
-              return (
-                <li key={p.name} className="tw-pt-4">
-                  <a href={p.url} target="_blank">{p.name}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
-    }
-  }
-  social() {
-    const social = [];
-    if (this.props.social.github) {
-      social.push(
-        <a key="1" href={`https://github.com/${this.props.social.github}`} target="_blank">
-          <img className="tw-w-12 lg:tw-w-16" src={githubIcon} />
+const Social = function(props) {
+  const social = [];
+  const itemCls = 'tw-float-left';
+  if (props.social.twitter) {
+    social.push(
+      <li key="2" className={itemCls}>
+        <a href={`https://twitter.com/${props.social.twitter}`} target="_blank">
+          <img alt="Twitter" className="tw-w-12 lg:tw-w-16" src={twitterIcon} />
         </a>
-      );
-    }
-    if (this.props.social.twitter) {
-      social.push(
-        <a key="2" href={`https://twitter.com/${this.props.social.twitter}`} target="_blank">
-          <img className="tw-w-12 lg:tw-w-16" src={twitterIcon} />
-        </a>
-      );
-    }
-    if (this.props.social.email) {
-      social.push(
-        <a key="3" href={`mailto:${this.props.social.email}`}>
-          <img className="tw-w-12 lg:tw-w-16" src={envelopeIcon} />
-        </a>
-      );
-    }
-    if (social.length) {
-      return (
-        <div className="tw-flex tw-justify-center lg:tw-justify-start tw-pt-4 lg:tw-pt-8">
-          {social}
-        </div>
-      );
-    }
-  }
-  render() {
-    return (
-      <nav className="tw-bg-black tw-text-white tw-w-full lg:tw-fixed lg:tw-pin-l lg:tw-pin-y lg:tw-w-96 tw-overflow-y-scroll">
-        <div className="tw-p-8">
-          <h1 className="tw-text-3xl lg:tw-text-4.5xl tw-font-thin tw-text-center lg:tw-text-left">
-            <Link to="/">{this.props.title}</Link>
-          </h1>
-          {this.projects()}
-          {this.social()}
-        </div>
-      </nav>
+      </li>
     );
   }
-}
-
-export default class Layout extends React.Component {
-  render() {
+  if (props.social.github) {
+    social.push(
+      <li key="1" className={itemCls}>
+        <a href={`https://github.com/${props.social.github}`} target="_blank">
+          <img alt="GitHub" className="tw-w-12 lg:tw-w-16" src={githubIcon} />
+        </a>
+      </li>
+    );
+  }
+  if (props.social.email) {
+    social.push(
+      <li key="3" className={itemCls}>
+        <a href={`mailto:${props.social.email}`}>
+          <img alt="Email" className="tw-w-12 lg:tw-w-16" src={envelopeIcon} />
+        </a>
+      </li>
+    );
+  }
+  if (social.length) {
     return (
-      <div className="tw-flex tw-flex-col tw-min-h-screen tw-font-sans">
-        <Nav {...this.props} />
-        <main className="lg:tw-ml-96 tw-p-4">
-          {this.props.children}
-        </main>
+      <div className="tw-flex tw-justify-center lg:tw-justify-start tw-pt-4 lg:tw-pt-8">
+        <ul className="tw-list-reset">
+          {social}
+        </ul>
       </div>
     );
   }
-}
+  return social;
+};
+
+const Projects = function(props) {
+  const [expanded, setExpanded] = useState(false);
+  if (!props.projects || !props.projects.length) {
+    return null;
+  }
+  let lsCls = 'tw-list-reset lg:tw-pl-4 lg:tw-block';
+  if (!expanded) {
+    lsCls += ' tw-hidden';
+  }
+  return (
+    <div className="tw-text-center lg:tw-text-left tw-text-2xl tw-font-thin tw-pt-4 lg:tw-pt-8">
+      <a className="tw-cursor-pointer lg:tw-cursor-default tw-pt-4" onClick={() => setExpanded(!expanded)}>
+        <span className="tw-hidden lg:tw-inline">Projects</span>
+        <span className="lg:tw-hidden">Projects {expanded ? '▲' : '▼'}</span>
+      </a>
+      <ul className={lsCls}>
+        {props.projects.map((p) => {
+          return (
+            <li key={p.name} className="tw-pt-4">
+              <a href={p.url} target="_blank">{p.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const Nav = function(props) {
+  return (
+    <nav className="tw-bg-black tw-text-white tw-w-full lg:tw-fixed lg:tw-pin-l lg:tw-pin-y lg:tw-w-96 tw-overflow-y-scroll">
+      <div className="tw-p-8">
+        <h1 className="tw-text-3xl lg:tw-text-4.5xl tw-font-thin tw-text-center lg:tw-text-left">
+          <Link to="/">{props.title}</Link>
+        </h1>
+        <Projects {...props} />
+        <Social {...props} />
+      </div>
+    </nav>
+  );
+};
+
+const Layout = function(props) {
+  return (
+    <div className="tw-flex tw-flex-col tw-min-h-screen tw-font-sans">
+      <Nav {...props} />
+      <main className="lg:tw-ml-96 tw-p-4">
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,41 +4,39 @@ import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark;
-    const { previous, next } = this.props.pageContext;
+const BlogPostTemplate = function(props) {
+  const post = props.data.markdownRemark;
+  const { previous, next } = props.pageContext;
 
-    return (
-      <Layout {...this.props.data.site.siteMetadata} location={this.props.location}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <h1 className="tw-text-3xl tw-font-light">{post.frontmatter.title}</h1>
-        <div className="tw-mt-1 tw-text-grey-dark tw-font-light">
-          {post.frontmatter.date}
-        </div>
-        <div className="tw-mt-4 tw-text-lg tw-font-light tw-leading-normal"
-          dangerouslySetInnerHTML={{ __html: post.html }} />
-        <br />
-        <br />
-        {previous && (
-          <Link to={previous.fields.slug} rel="prev">
-            ← {previous.frontmatter.title}
-          </Link>
-        )}
-        <br />
-        <br />
-        {next && (
-          <Link className="tw-block tw-text-right" to={next.fields.slug} rel="next">
-            {next.frontmatter.title} →
-          </Link>
-        )}
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout {...props.data.site.siteMetadata} location={props.location}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <h1 className="tw-text-3xl tw-font-light">{post.frontmatter.title}</h1>
+      <div className="tw-mt-1 tw-text-grey-dark tw-font-light">
+        {post.frontmatter.date}
+      </div>
+      <div className="tw-mt-4 tw-text-lg tw-font-light tw-leading-normal"
+        dangerouslySetInnerHTML={{ __html: post.html }} />
+      <br />
+      <br />
+      {previous && (
+        <Link to={previous.fields.slug} rel="prev">
+          ← {previous.frontmatter.title}
+        </Link>
+      )}
+      <br />
+      <br />
+      {next && (
+        <Link className="tw-block tw-text-right" to={next.fields.slug} rel="next">
+          {next.frontmatter.title} →
+        </Link>
+      )}
+    </Layout>
+  );
+};
 
 export default BlogPostTemplate;
 
